9f4fc3627e48a1073887915f4a9dd175aa339fe0
agorski <adriangorski996@gmail.com>
2020-07-20 19:31:34 +0200

    deploy script typo fix

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ docker push invicit/multi-worker:latest
 docker push invicit/multi-client:$SHA
 docker push invicit/multi-server:$SHA
 docker push invicit/multi-worker:$SHA
-kubectk apply -f ./k8s
+kubectl apply -f ./k8s
 kubectl set image deplotments/server-deployment server=invicit/multi-server:$SHA
 kubectl set image deplotments/client-deployment client=invicit/multi-client:$SHA
 kubectl set image deplotments/worker-deployment worker=invicit/multi-worker:$SHA
